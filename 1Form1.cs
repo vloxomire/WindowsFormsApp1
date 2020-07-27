ba012e5e28b5b3e8326bc70ca264fec0b121205c
Max Sebastián Saldaña <vloxomire@gmail.com>
2020-07-26 21:33:04 -0300

    Initial commit

---
+++ 1Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 =int.Parse(textBox1.Text);
+            int num2 =int.Parse(textBox2.Text);
+            label4.Text = (num1 + num2).ToString();
+        }
+    }
+}
---
+++ 2Password.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Password : Form
+    {
+        private string clave;
+        private StreamWriter streamwriter;
+        public Password()
+        {
+            InitializeComponent();
+            streamwriter = new StreamWriter(@"C:\Users\vloxo\Documents\clave.txt", false);
+            this.clave = "123abc";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text!=clave)
+            {
+                label2.Text = "Clave incorrecta";
+                this.BackColor = System.Drawing.SystemColors.Menu;
+                return;
+            }
+            label2.Text = "Clave correcta";
+            streamwriter.WriteLine("Clave: "+textBox1.Text);
+            streamwriter.Close();
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+        }
+    }
+}
